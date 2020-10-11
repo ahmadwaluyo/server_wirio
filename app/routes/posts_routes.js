84c7f5eb87812e44f7147a5c5ee95d9dd9878646
Piscki F. Pratama <pisckipratama@gmail.com>
2020-10-11 22:06:44 +0700

    add get by id, create, delete, update post

--- app/routes/posts_routes.js
+++ app/routes/posts_routes.js
@@ -4,5 +4,8 @@ const { protects } = require('../middlewares/auth');
 
 router.post('/', protects, PostController.createPost);
 router.get('/', PostController.getPosts);
+router.get('/:id', PostController.getPostById);
+router.put('/:id', PostController.updatePost);
+router.delete('/:id', PostController.deletePost);
 
 module.exports = router;
